04db466e51ee672c50a091a257fb2e1a5e82174b
Min Jeong Kim <minnjeong.kim@gmail.com>
2023-11-13 14:39:21 +0900

    [MOD] pages 폴더 복구

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,11 @@
-import Intro from '@/features/intro/Intro';
-import Login from '@/features/login/Login';
-import Signup from '@/features/signup/Signup';
-import UserSetting from '@/features/userSetting/UserSetting';
+import Intro from '@/pages/IntroPage';
+import Login from '@/pages/LoginPage';
+import Signup from '@/pages/SignupPage';
+import UserSetting from '@/pages/UserSettingPage';
+import FeedCreatePage from '@/pages/feed/FeedCreatePage';
+import FeedDetailPage from '@/pages/feed/FeedDetailPage';
+import FeedEditPage from '@/pages/feed/FeedEditPage';
+import FeedListPage from '@/pages/feed/FeedListPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Router = () => {
@@ -14,12 +18,13 @@ const Router = () => {
         <Route path="/login" element={<Login />} />
 
         <Route path="/feed">
-          <Route path="/feed/create" />
-          <Route path="/feed/:id/modify" />
-          <Route path="/feed/:id" />
+          <Route path="/feed/list" element={<FeedListPage />} />
+          <Route path="/feed/create" element={<FeedCreatePage />} />
+          <Route path="/feed/:id/edit" element={<FeedEditPage />} />
+          <Route path="/feed/:id" element={<FeedDetailPage />} />
         </Route>
 
-        <Route path="/account/setting" element={<UserSetting />} />
+        <Route path="/user/setting" element={<UserSetting />} />
       </Routes>
     </BrowserRouter>
   );
