0e2676ae0fe05a92e54207a789a77af6b0e75aa2
Min Jeong Kim <minnjeong.kim@gmail.com>
2023-12-01 17:08:31 +0900

    [MOD] Layout UI 수정

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,6 +3,12 @@ import { Header } from './Layout.styles';
 import { Button } from '..';
 import { useNavigate } from 'react-router';
 import { auth } from '@/service/firebase';
+import { PrevArrow } from '@/assets/icons/PrevArrow';
+import Menu from '../Menu/Menu';
+import { MenuListType } from '../Menu/Menu.types';
+import { MenuBox } from '../Menu/Menu.styles';
+import Profile from '../Profile/Profile';
+import { User } from 'firebase/auth';
 
 interface LayoutProps {
   children: ReactNode;
@@ -11,6 +17,8 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
 
   const navigate = useNavigate();
 
@@ -23,13 +31,26 @@ const Layout = ({ children }: LayoutProps) => {
   const checkCurrentUser = () => {
     auth.onAuthStateChanged((user) => {
       if (user) {
+        console.log('user', user);
         setIsLoggedIn(true);
+        setCurrentUser(user);
       } else {
         setIsLoggedIn(false);
       }
       setIsLoading(true);
     });
   };
+
+  const menuList: MenuListType[] = [
+    {
+      name: '마이페이지',
+      onClick: () => navigate('/user/setting'),
+    },
+    {
+      name: '로그아웃',
+      onClick: onClickLogOut,
+    },
+  ];
 
   useEffect(() => {
     checkCurrentUser();
@@ -45,6 +66,14 @@ const Layout = ({ children }: LayoutProps) => {
         }}
       >
         <Header>
+          <div
+            style={{
+              cursor: 'pointer',
+            }}
+            onClick={() => navigate(-1)}
+          >
+            <PrevArrow color="#121212" />
+          </div>
           <div
             onClick={() => navigate('/intro')}
             style={{
@@ -70,14 +99,15 @@ const Layout = ({ children }: LayoutProps) => {
               </Button>
             </div>
           ) : (
-            <div>
-              <Button $border="1px solid #D3D3D3" $marginRight="8px" onClick={onClickLogOut}>
-                로그아웃
-              </Button>
-              <Button $background="#f0133a" $color="#fff" onClick={() => navigate('/feed/create')}>
-                글 작성하기
-              </Button>
-            </div>
+            <MenuBox>
+              <Profile
+                url={currentUser?.photoURL ?? ''}
+                name={currentUser?.displayName ?? ''}
+                onlyImg
+                onClick={() => setOpenMenu(!openMenu)}
+              />
+              {openMenu && <Menu list={menuList} $top={50} />}
+            </MenuBox>
           )}
         </Header>
       </div>
